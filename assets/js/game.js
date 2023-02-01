9bd137099547a575c2969245caac218d56219c43
IsaiasQB <isaias.qb@hotmail.com>
2023-02-01 02:12:07 -0500

    added the option to check on enemy's and player's health

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,7 @@ console.log(
 
 var enemyName = "Robort0";
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 120;
 
 
 var fight = function(){
@@ -19,13 +19,24 @@ var fight = function(){
   // Substract playerAttack form Enemy's health
   enemyHealth = enemyHealth - playerAttack
   // Log resulting Enemy's health
-  console.log(`FIRST STRIKE!
-  ${playerName} Attacked. ${enemyName}'s health is now ${enemyHealth} `)
+  console.log(`FIRST STRIKE:  ${playerName} Attacked!`)
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(`${enemyName} has died`)
+  } else {
+    window.alert(`${enemyName}'s health is now ${enemyHealth}`)
+  }
+
   // Substract enemyAttack from Player's health
   playerHealth = playerHealth - enemyAttack
   // Log resulting player's health
-  console.log(`SECOND STRIKE!
-  ${enemyName} Attacked. ${playerName}'s health is now ${playerHealth} `)
+  console.log(`SECOND STRIKE: ${enemyName} Attacked!`)
+  // check our robot's health
+  if (playerHealth <= 0){
+    window.alert(`${playerName} has died`)
+  } else {
+    window.alert(`${playerName}'s health is now ${playerHealth}`)
+  }
 }
 
 fight();
