6274f92ca4d63a26491e20cbbb09e570e6049704
IsaiasQB <isaias.qb@hotmail.com>
2023-02-02 01:36:19 -0500

    added penalization code for player's who chose to skip the battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,16 @@
-
+// color variables, for console output text
+var clr = '\x1B[31m' 
+var endclr = '\x1B[0m'
 
 var playerName = window.prompt("What is your CHAMPION's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(
-  `${playerName} is ready to fight!
-  he has ${playerHealth} health points 
-  and packs a punch of ${playerAttack} megatons!`);
+  `${clr}${playerName}${endclr} is ready to fight!
+  he has ${clr}${playerHealth}${endclr} health points 
+  and packs a punch of ${clr}${playerAttack} megatons!${endclr}`);
 
 var enemyName = "Robort0";
 var enemyHealth = 50;
@@ -16,7 +19,6 @@ var enemyAttack = 12;
 
 var fight = function(){
   window.alert("WELCOME to ROBOALERT Battle!");
-
   // ask player if they want to fight or skip battle
   var promptFight = window.prompt(`Would you FIGHT or SKIP this battle?
   enter FIGHT or SKIP to choose.`);
@@ -24,11 +26,11 @@ var fight = function(){
   //IF player chooses to fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     //log the answer of the skip or fight
-    console.log(`You have chosen to FIGHT this battle`);
+    console.log(`You chose to ${clr}FIGHT${endclr} this battle!`);
     // Substract playerAttack form Enemy's health
     enemyHealth = enemyHealth - playerAttack
     // Log resulting Enemy's health
-    console.log(`FIRST STRIKE:  ${playerName} Attacked!`)
+    console.log(`YOU STRIKE: ${playerName} Attacked!`)
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(`${enemyName} has died`)
@@ -39,16 +41,28 @@ var fight = function(){
     // Substract enemyAttack from Player's health
     playerHealth = playerHealth - enemyAttack
     // Log resulting player's health
-    console.log(`SECOND STRIKE: ${enemyName} Attacked!`)
+    console.log(`ENEMY STRIKES: ${enemyName} Attacked!`)
     // check player's health
     if (playerHealth <= 0){
       window.alert(`${playerName} has died`)
     } else {
       window.alert(`${playerName}'s health is now ${playerHealth}`)
     }
+
   } // IF player chooses to skip the battle
   else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(`${playerName} has chosen to SKIP this battle!`)
+    // confirm to skip battle
+    var confirmSkip = window.confirm(`Are you sure you want to SKIP the Battle?`);
+      // if YES (true), leave fight
+      if (confirmSkip) {
+        playerMoney = playerMoney - 2
+        window.alert(`${playerName} has chosen to SKIP this battle!`)
+        console.log(`You haven been Penalized for skipping the battle. Your remaining Money is ${clr}${playerMoney} coins `)
+      } // if NO (false), ask question again. Run fight()
+      else {
+        fight()
+      }
+      
   } // IF player enters work answer 
   else {
     window.alert("Please choose a valid option");
