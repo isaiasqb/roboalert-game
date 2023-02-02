4646d0aaf8f594b9c0e1916de66b8699ba492ac9
IsaiasQB <isaias.qb@hotmail.com>
2023-02-02 00:29:44 -0500

    added the ability to ask if player wants to fight or skip battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 
 
-var playerName = window.prompt("What is your name?");
+var playerName = window.prompt("What is your CHAMPION's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
@@ -11,31 +11,47 @@ console.log(
 
 var enemyName = "Robort0";
 var enemyHealth = 50;
-var enemyAttack = 120;
+var enemyAttack = 12;
 
 
 var fight = function(){
   window.alert("WELCOME to ROBOALERT Battle!");
-  // Substract playerAttack form Enemy's health
-  enemyHealth = enemyHealth - playerAttack
-  // Log resulting Enemy's health
-  console.log(`FIRST STRIKE:  ${playerName} Attacked!`)
-  // check enemy's health
-  if (enemyHealth <= 0) {
-    window.alert(`${enemyName} has died`)
-  } else {
-    window.alert(`${enemyName}'s health is now ${enemyHealth}`)
-  }
 
-  // Substract enemyAttack from Player's health
-  playerHealth = playerHealth - enemyAttack
-  // Log resulting player's health
-  console.log(`SECOND STRIKE: ${enemyName} Attacked!`)
-  // check our robot's health
-  if (playerHealth <= 0){
-    window.alert(`${playerName} has died`)
-  } else {
-    window.alert(`${playerName}'s health is now ${playerHealth}`)
+  // ask player if they want to fight or skip battle
+  var promptFight = window.prompt(`Would you FIGHT or SKIP this battle?
+  enter FIGHT or SKIP to choose.`);
+
+  //IF player chooses to fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+    //log the answer of the skip or fight
+    console.log(`You have chosen to FIGHT this battle`);
+    // Substract playerAttack form Enemy's health
+    enemyHealth = enemyHealth - playerAttack
+    // Log resulting Enemy's health
+    console.log(`FIRST STRIKE:  ${playerName} Attacked!`)
+    // check enemy's health
+    if (enemyHealth <= 0) {
+      window.alert(`${enemyName} has died`)
+    } else {
+      window.alert(`${enemyName}'s health is now ${enemyHealth}`)
+    }
+    
+    // Substract enemyAttack from Player's health
+    playerHealth = playerHealth - enemyAttack
+    // Log resulting player's health
+    console.log(`SECOND STRIKE: ${enemyName} Attacked!`)
+    // check player's health
+    if (playerHealth <= 0){
+      window.alert(`${playerName} has died`)
+    } else {
+      window.alert(`${playerName}'s health is now ${playerHealth}`)
+    }
+  } // IF player chooses to skip the battle
+  else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(`${playerName} has chosen to SKIP this battle!`)
+  } // IF player enters work answer 
+  else {
+    window.alert("Please choose a valid option");
   }
 }
 
